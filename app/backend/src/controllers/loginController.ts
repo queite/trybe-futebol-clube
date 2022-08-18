8837a564f1b9887b4e0ca053c41773390e8c7dfb
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-08-18 19:20:19 -0300

    :sparkles: feat: add login and password validation

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -2,11 +2,14 @@ import { Request, Response } from 'express';
 import jwtService from '../services/jwtService';
 import ILoginService from '../interfaces/ILoginService';
 import IUser from '../interfaces/IUser';
+import BadRequestError from '../errors/badRequestError';
 
 export default class LoginController {
   constructor(private userService: ILoginService<IUser>) {}
 
   public login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    if (!email || !password) throw new BadRequestError('All fields must be filled');
     const user = await this.userService.login(req.body);
     const token = jwtService.sign(user.email);
     return res.status(200).json({ token });
