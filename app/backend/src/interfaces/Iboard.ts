f9e9ed8237f965a92b7d3641aef3ce9f22ef2f49
Queite Schneider Castiglioni <queitesc@gmail.com>
2022-08-23 08:17:53 -0300

    :construction: feat: finish route /leaderboard/home

--- app/backend/src/interfaces/Iboard.ts
+++ app/backend/src/interfaces/Iboard.ts
@@ -1,5 +1,5 @@
 export default interface IBoard {
-  name: string
+  name?: string
   totalPoints:number
   totalGames: number
   totalVictories: number
--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -1,3 +1,4 @@
+import IBoard from '../interfaces/Iboard';
 import IMatch from '../interfaces/IMatch';
 import MatchService from './matchService';
 import TeamService from './teamService';
@@ -8,25 +9,34 @@ export default class LeaderboardService {
     let totalVictories = 0;
     let totalDraws = 0;
     let totalLosses = 0;
-
     match.forEach((element) => {
       if (element.homeTeamGoals > element.awayTeamGoals) {
         totalPoints += 3;
         totalVictories += 1;
-      }
-      if (element.homeTeamGoals === element.awayTeamGoals) {
+      } else if (element.homeTeamGoals === element.awayTeamGoals) {
         totalPoints += 1;
         totalDraws += 1;
+      } else if (element.homeTeamGoals < element.awayTeamGoals) {
+        totalPoints += 0;
+        totalLosses += 1;
       }
-      totalPoints += 0;
-      totalLosses += 1;
     });
-
     return { totalPoints, totalVictories, totalDraws, totalLosses };
   }
 
   public static efficiencyCalc(points: number, matches: number) {
     return Number(((points / (matches * 3)) * 100).toFixed(2));
+  }
+
+  public static goalsCalc(matches: IMatch[]) {
+    let goalsFavor = 0;
+    let goalsOwn = 0;
+    matches.forEach((match) => {
+      goalsFavor += match.homeTeamGoals;
+      goalsOwn += match.awayTeamGoals;
+    });
+
+    return { goalsFavor, goalsOwn, goalsBalance: goalsFavor - goalsOwn };
   }
 
   public static async getResults() {
@@ -39,10 +49,10 @@ export default class LeaderboardService {
         .filter((match: IMatch) => team.teamName === match.teamHome?.teamName);
       return this.createResults(teamMatches);
     }));
-    return results;
+    return this.orderBoard(results);
   }
 
-  public static async createResults(matches: IMatch[]) {
+  public static createResults(matches: IMatch[]) {
     return {
       name: matches[0].teamHome?.teamName,
       totalPoints: this.totalPointsCalc(matches).totalPoints,
@@ -50,10 +60,18 @@ export default class LeaderboardService {
       totalVictories: this.totalPointsCalc(matches).totalVictories,
       totalDraws: this.totalPointsCalc(matches).totalDraws,
       totalLosses: this.totalPointsCalc(matches).totalLosses,
-      // goalsFavor: number,
-      // goalsOwn: number,
-      // goalsBalance: number,
-      // efficiency: number,
+      goalsFavor: this.goalsCalc(matches).goalsFavor,
+      goalsOwn: this.goalsCalc(matches).goalsOwn,
+      goalsBalance: this.goalsCalc(matches).goalsBalance,
+      efficiency: this.efficiencyCalc(this.totalPointsCalc(matches).totalPoints, matches.length),
     };
+  }
+
+  public static orderBoard(results: IBoard[]) {
+    return results.sort((a: IBoard, b: IBoard) => b.totalPoints - a.totalPoints
+      || b.totalVictories - a.totalVictories
+      || b.goalsBalance - a.goalsBalance
+      || b.goalsFavor - a.goalsFavor
+      || a.goalsOwn - b.goalsOwn);
   }
 }
